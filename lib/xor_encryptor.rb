18b0faca002b34f681ada5c17efb1969f7590d61
toma <t0ma.popov.90@gmail.com>
2015-12-01 16:25:57 +0200

    rework encode/decode; now works with pairs of 3 characters

--- lib/xor_encryptor.rb
+++ lib/xor_encryptor.rb
@@ -3,18 +3,26 @@ require "xor_encryptor/core_extentions"
 
 module XorEncryptor
   class << self
-    def process(string, key)
+    def encode(string, key)
       #We use m as a modifier for each string sequence
       if key.size < string.size
         mult = (string.size / key.size) + 1
         key =(key * mult)[0..(string.size - 1)]
       end
 
-      string.split("").zip(key.split("")).map{|rc| (rc[0].ord ^ rc[1].ord).chr }.join
+      string.split("").zip(key.split("")).map{|rc| (rc[0].ord ^ rc[1].ord).to_s.rjust(3, "0") }.join
     end
 
-    alias :decode :process
-    alias :encode :process
+    def decode(string, key)
+      string_array = string.split("").to_a.each_slice(3).map(&:join)
+
+      if key.size < string.size
+        mult = (string.size / key.size) + 1
+        key =(key * mult)[0..(string.size - 1)]
+      end
+
+      string_array.zip(key.split("")).map{|rc| (rc[0].to_i ^ rc[1].ord).chr }.join
+    end
 
     def self_xor_digest(string)
       string.split("").reduce("0"){|c, x| c.to_s.ord ^ x.ord }
