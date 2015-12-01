2e5f8eb6dbd387bbf65dfac2e4b018b85c1a9e0e
toma <t0ma.popov.90@gmail.com>
2015-12-01 17:29:32 +0200

    Bugfix in decode

--- lib/xor_encryptor.rb
+++ lib/xor_encryptor.rb
@@ -4,24 +4,21 @@ require "xor_encryptor/core_extentions"
 module XorEncryptor
   class << self
     def encode(string, key)
-      #We use m as a modifier for each string sequence
-      if key.size < string.size
-        mult = (string.size / key.size) + 1
-        key =(key * mult)[0..(string.size - 1)]
-      end
-
+      key = modify_key string, key
       string.split("").zip(key.split("")).map{|rc| (rc[0].ord ^ rc[1].ord).to_s.rjust(3, "0") }.join
     end
 
     def decode(string, key)
       string_array = string.split("").to_a.each_slice(3).map(&:join)
-
-      if key.size < string.size
-        mult = (string.size / key.size) + 1
-        key =(key * mult)[0..(string.size - 1)]
-      end
-
+      key = modify_key string_array, key
       string_array.zip(key.split("")).map{|rc| (rc[0].to_i ^ rc[1].ord).chr }.join
+    end
+
+    def modify_key(obj, key)
+      return key if key.size >= obj.size
+
+      mult = (obj.size / key.size) + 1
+      (key * mult)[0..(obj.size - 1)]
     end
 
     def self_xor_digest(string)
